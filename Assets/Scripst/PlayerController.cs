097335bc7e9bb32745332dc180f1126305e88f40
Luisa <lsaenzroa@gmail.com>
2025-03-11 12:31:15 -0500

    add player movement and animation

--- Assets/Scripst/PlayerController.cs
+++ Assets/Scripst/PlayerController.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    private float movementX; 
+    private Rigidbody2D playerRb;
+    private bool isGrounded;
+    private SpriteRenderer spriteRenderer;
+     private Animator animator; // Referencia al Animator
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+        animator = GetComponent<Animator>(); 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+
+
+    }
+
+    void PlayerMovement()
+    {
+        {
+        movementX = Input.GetAxis("Horizontal"); // Usamos la variable global
+        playerRb.velocity = new Vector2(movementX * speed, playerRb.velocity.y);
+
+         // Controlar animaciones
+        animator.SetFloat("Speed", Mathf.Abs(movementX));
+
+        if (movementX > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+            animator.SetBool("IsJumping", true);
+        }
+    }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
