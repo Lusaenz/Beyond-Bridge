4cf909f6d918b2b784a2f5ea610f159aadf59777
FebaDev <felipebposada@gmail.com>
2025-03-11 16:56:11 -0500

    BridgeOne, SpawnDrops

---
+++ Assets/Scripst/ActivateBridge.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateBridge : MonoBehaviour
+{
+    public Animator bridgeAnimator;
+    [SerializeField]private bool playerInZone = false;
+    [SerializeField]private bool friendInZone = false;
+
+    private string playerTag = "Player";
+    private string friendTag = "Friend";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag(playerTag))
+        {
+            playerInZone = true;
+        }
+        else if(other.CompareTag(friendTag)) 
+        {
+            friendInZone = true;
+        }
+
+        if (playerInZone && friendInZone)
+        {
+            ActivateBridgeAnim();
+        }
+    }
+    private void ActivateBridgeAnim()
+    {
+        // Activa la animación del puente
+        if (bridgeAnimator != null)
+        {
+            bridgeAnimator.SetTrigger("ActivateBridge");
+        }
+    }
+}
