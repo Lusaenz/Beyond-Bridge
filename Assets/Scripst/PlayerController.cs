16540f38d94bdb88427313feeb069f2dfa1cf8e4
Luisa <lsaenzroa@gmail.com>
2025-03-11 12:33:24 -0500

    add player to prefabs

--- Assets/Scripst/PlayerController.cs
+++ Assets/Scripst/PlayerController.cs
@@ -1,56 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 7f;
-    private float movementX; 
+    private float movementX;
     private Rigidbody2D playerRb;
     private bool isGrounded;
     private SpriteRenderer spriteRenderer;
-     private Animator animator; // Referencia al Animator
+    private Animator animator;
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>(); 
-        animator = GetComponent<Animator>(); 
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        PlayerMovement();
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
 
     }
 
     void PlayerMovement()
     {
         {
-        movementX = Input.GetAxis("Horizontal"); // Usamos la variable global
-        playerRb.velocity = new Vector2(movementX * speed, playerRb.velocity.y);
+            movementX = Input.GetAxis("Horizontal"); 
+            playerRb.velocity = new Vector2(movementX * speed, playerRb.velocity.y);
 
-         // Controlar animaciones
-        animator.SetFloat("Speed", Mathf.Abs(movementX));
+            // Controlar animaciones
+            animator.SetFloat("Speed", Mathf.Abs(movementX));
 
-        if (movementX > 0)
-        {
-            spriteRenderer.flipX = false;
-        }
-        else if (movementX < 0)
-        {
-            spriteRenderer.flipX = true;
-        }
+            if (movementX > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (movementX < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
-            animator.SetBool("IsJumping", true);
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+                animator.SetBool("IsJumping", true);
+            }
         }
-    }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
