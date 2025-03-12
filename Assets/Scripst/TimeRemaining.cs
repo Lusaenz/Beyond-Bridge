7b78c2a7034b9e5d7f7ae92dcdbc364f7cbb24c5
FebaDev <felipebposada@gmail.com>
2025-03-11 22:54:53 -0500

    World Gameplay Creation

--- Assets/Scripst/TimeRemaining.cs
+++ Assets/Scripst/TimeRemaining.cs
@@ -7,20 +7,37 @@ public class TimeRemaining : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
+    private bool timerActive = false;  // Variable para controlar cuando el temporizador comienza a contar
 
     private void Update()
     {
-        if (remainingTime > 0)
+        // Si el temporizador está activo y el tiempo restante es mayor que cero, comienza a contar
+        if (timerActive)
         {
-            remainingTime -= Time.deltaTime;
-        }
-        else if (remainingTime < 0)
-        {
-            remainingTime = 0;
-        }
+            if (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+            }
+            else if (remainingTime < 0)
+            {
+                remainingTime = 0;
+            }
 
-        int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("Time Remaining: {0:00}:{1:00}", minutes, seconds);
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("Time Remaining: {0:00}:{1:00}", minutes, seconds);
+        }
+    }
+
+    // Método para activar el temporizador
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+
+    // Método para detener el temporizador, si lo necesitas
+    public void StopTimer()
+    {
+        timerActive = false;
     }
 }
