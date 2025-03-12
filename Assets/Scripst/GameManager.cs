72b352ee39c7bca08c67d6c7b257fc381919b78e
Davidoch13 <davidospina0913@gmail.com>
2025-03-11 19:23:54 -0500

    Game Manager created

--- Assets/Scripst/GameManager.cs
+++ Assets/Scripst/GameManager.cs
@@ -1,18 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager GameInstance { get; private set; }
+    public bool isPaused;
+
+    public event Action OnWin;
+    public event Action OnLose;
+
+    private void Awake()
     {
-        
+        if (GameInstance == null)
+        {
+            GameInstance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameOverWin()
     {
-        
+        Debug.Log("You win!");
+        isPaused = true;
+        Time.timeScale = 0;
+        OnWin?.Invoke();
+    }
+
+    public void GameOverLose()
+    {
+        Debug.Log("You lose!");
+        isPaused = true;
+        Time.timeScale = 0;
+        OnLose?.Invoke();
+    }
+
+    public void PauseGame()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+
+    public void ReloadScene()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+
+        AudioManager.Instance.musicSource.Stop();
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        AudioManager.Instance.PlayMusic("Theme");
     }
 }
